196f67296a135707ab25f1ac6ebaecbdadabc656
Paneesh <11010866@stud.hochschule-heidelberg.de>
2019-02-20 23:50:38 +0100

    kijnkjnkj

---
+++ ReverseEngineering/src/test/java/com/reverse/engineering/ReverseEngineeringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.reverse.engineering;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReverseEngineeringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
